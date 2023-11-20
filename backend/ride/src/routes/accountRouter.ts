39ab561a8458d889c5d1c5f34f5bc13a72980426
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2023-11-20 15:05:29 -0300

    arcabouço inicial da aula 2

--- backend/ride/src/routes/accountRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAccountController, signupController } from "../controllers/accountController";
-
-const accountRouter = Router();
-accountRouter.get('/account/:uuid', getAccountController);
-accountRouter.post('/signup', signupController);
-
-export default accountRouter;
--- backend/ride/src/routes/rideRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getRideController, requestRideController } from "../controllers/rideController";
-
-const rideRouter = Router();
-rideRouter.get('/ride/:uuid', getRideController);
-rideRouter.post('/ride', requestRideController);
-
-export default rideRouter;
