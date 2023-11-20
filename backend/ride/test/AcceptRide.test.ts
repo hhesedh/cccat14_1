c0a5fb78a37bb580ee922757f264293e02e05169
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2023-11-20 16:38:38 -0300

    refatoração  e criação do segundo teste de regra de negócio

--- backend/ride/test/AcceptRide.test.ts
+++ backend/ride/test/AcceptRide.test.ts
@@ -13,6 +13,43 @@ let requestRide: RequestRide;
 let acceptRide: AcceptRide;
 let getRide: GetRide;
 
+async function createRequestRide(requestRide: RequestRide, accountId: string) {
+	const inputRequestRide = {
+		passengerId: accountId,
+		fromLat: -27.584905257808835,
+		fromLong: -48.545022195325124,
+		toLat: -27.496887588317275,
+		toLong: -48.522234807851476
+	};
+
+	return requestRide.execute(inputRequestRide);
+}
+
+async function signupPassenger(signup: Signup) {
+	const passenger = createUser("Jhon Passageiro", true);
+	return signup.execute(passenger);
+
+}
+
+async function signupDriver(signup: Signup) {
+	const driver = createUser("Fabio Motorista", false);
+	return signup.execute(driver);
+}
+
+
+function createUser(name: string, isPassenger: boolean) {
+	const credencial = isPassenger ? 'passageiro' : 'motorista';
+	const email = `${name.split(" ")[0].toLowerCase()}.${credencial};`
+	return {
+		name,
+		email: `${email}${Math.random()}@gmail.com`,
+		cpf: "97456321558",
+		isPassenger,
+		isDriver: !isPassenger,
+		password: "123456"
+	};
+}
+
 beforeEach(() => {
 	const accountDAO = new AccountDAODatabase();
 	const rideDAO = new RideDAODatabase();
@@ -26,37 +63,18 @@ beforeEach(() => {
 
 
 test("Deve verificar se o account_id tem is_driver true", async () => {
-
-	const DriverInputSignup = {
-		name: "Fabio jhon",
-		email: `john.fabio${Math.random()}@gmail.com`,
-		cpf: "97456321558",
-		isPassenger: true,
-		password: "123456"
-	};
-	const driverOutputSignup = await signup.execute(DriverInputSignup);
-	const passengerInputSignup = {
-		name: "John Doe",
-		email: `john.doe${Math.random()}@gmail.com`,
-		cpf: "97456321558",
-		isPassenger: true,
-		password: "123456"
-	};
-	const passengerOutputSignup = await signup.execute(passengerInputSignup);
-	const inputRequestRide = {
-		passengerId: passengerOutputSignup.accountId,
-		fromLat: -27.584905257808835,
-		fromLong: -48.545022195325124,
-		toLat: -27.496887588317275,
-		toLong: -48.522234807851476
-	};
-
-	const outputRequestRide = await requestRide.execute(inputRequestRide);
+	const driverOutputSignup = await signupPassenger(signup);
+	const passengerOutputSignup = await signupPassenger(signup);
+	const outputRequestRide = await createRequestRide(requestRide, passengerOutputSignup.accountId);
 
 	await expect(() => acceptRide.execute(outputRequestRide.rideId, driverOutputSignup.accountId)).rejects.toThrow(new Error("Conta deve ser de um motorista"));
+});
 
 
+test('Deve verificar se o status da corrida é "requested", se não for, lançar um erro', async () => {
+	const driverOutputSignup = await signupDriver(signup);
+	const passengerOutputSignup = await signupPassenger(signup);
+	const outputRequestRide = await createRequestRide(requestRide, passengerOutputSignup.accountId);
 
-
-
+	await expect(() => acceptRide.execute(outputRequestRide.rideId, driverOutputSignup.accountId)).rejects.toThrow(new Error("Corrida deve ter o status requested"));
 });
