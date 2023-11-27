558f40586d4e02ce992d662a837a994545c3edef
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2023-11-27 01:10:40 -0300

    alterações

--- backend/ride/src/AcceptRide.ts
+++ backend/ride/src/AcceptRide.ts
@@ -13,9 +13,12 @@ export default class AcceptRide {
 		const account = await this.AccountDAO.getById(driver_id);
 		if (!account?.is_driver) throw new Error("Conta deve ser de um motorista");
 		const ride = await this.rideDAO.getById(ride_id);
+
 		if (ride?.status !== 'requested') throw new Error("Corrida deve ter o status requested");
 		const driverRides = await this.rideDAO.getByDriverId(driver_id);
+		console.log(driverRides);
 		const driverHasPendentsRides = driverRides.some(ride => ride.status === 'accepted' || ride.status === 'in_progress');
+
 		if (driverHasPendentsRides) throw new Error("Motorista não pode ter outra corrida com status accepted ou in_progress");
 		await this.rideDAO.acceptRide(ride_id, driver_id);
 	}
--- backend/ride/test/AcceptRide.test.ts
+++ backend/ride/test/AcceptRide.test.ts
@@ -6,58 +6,16 @@ import LoggerConsole from "../src/LoggerConsole";
 import RequestRide from "../src/RequestRide";
 import RideDAODatabase from "../src/RideDAODatabase";
 import Signup from "../src/Signup";
+import StartRide from "../src/StartRide";
+import { createRequestRide, signupDriver, signupPassenger } from "./utils/rideUtils";
 
 let signup: Signup;
 let getAccount: GetAccount;
 let requestRide: RequestRide;
 let acceptRide: AcceptRide;
 let getRide: GetRide;
+let startRide: StartRide;
 
-async function createRequestRide(requestRide: RequestRide, accountId: string) {
-	const inputRequestRide = {
-		passengerId: accountId,
-		fromLat: -27.584905257808835,
-		fromLong: -48.545022195325124,
-		toLat: -27.496887588317275,
-		toLong: -48.522234807851476
-	};
-
-	return requestRide.execute(inputRequestRide);
-}
-
-async function signupPassenger(signup: Signup) {
-	const passenger = createUser("Jhon Passageiro", true);
-	return signup.execute(passenger);
-
-}
-
-async function signupDriver(signup: Signup) {
-	const driver = createUser("Fabio Motorista", false);
-	return signup.execute(driver);
-}
-
-
-function createUser(name: string, isPassenger: boolean) {
-	const credencial = isPassenger ? 'passageiro' : 'motorista';
-	const email = `${name.split(" ")[0].toLowerCase()}.${credencial};`
-	return {
-		name,
-		email: `${email}${Math.random()}@gmail.com`,
-		cpf: "97456321558",
-		isPassenger,
-		isDriver: !isPassenger,
-		password: "123456",
-		...(isPassenger ? {} : { carPlate: generateCarPlate() })
-	};
-}
-
-
-function generateCarPlate(): string {
-	const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-	const letrasAleatorias = Array(3).fill(null).map(() => letras[Math.floor(Math.random() * 26)]);
-	const numerosAleatorios = Array(4).fill(null).map(() => Math.floor(Math.random() * 10));
-	return [...letrasAleatorias, ...numerosAleatorios].join("");
-}
 
 beforeEach(() => {
 	const accountDAO = new AccountDAODatabase();
@@ -68,7 +26,8 @@ beforeEach(() => {
 	requestRide = new RequestRide(rideDAO, logger);
 	getRide = new GetRide(rideDAO, logger);
 	acceptRide = new AcceptRide(rideDAO, accountDAO, logger);
-})
+	startRide = new StartRide(rideDAO, logger);
+});
 
 
 test("Deve verificar se o account_id tem is_driver true", async () => {
@@ -87,11 +46,15 @@ test('Deve verificar se o status da corrida é "requested", se não for, lançar
 });
 
 // verificar essa condição
-test('deve verificar se o motorista já tem outra corrida com status "accepted" ou "in_progress", se tiver lançar um erro', async () => {
+test.only('deve verificar se o motorista já tem outra corrida com status "accepted" ou "in_progress", se tiver lançar um erro', async () => {
 	const driverOutputSignup = await signupDriver(signup);
 	const passengerOutputSignup = await signupPassenger(signup);
 	const outputRequestRide = await createRequestRide(requestRide, passengerOutputSignup.accountId);
-	await expect(() => acceptRide.execute(outputRequestRide.rideId, driverOutputSignup.accountId)).rejects.toThrow(new Error("Motorista não pode ter outra corrida com status accepted ou in_progress"));
+	await startRide.execute(outputRequestRide.rideId);
+	const otherPassengerOutputSignup = await signupPassenger(signup);
+	const secondOutputRequestRide = await createRequestRide(requestRide, otherPassengerOutputSignup.accountId);
+	await expect(() => acceptRide.execute(secondOutputRequestRide.rideId, driverOutputSignup.accountId))
+		.rejects.toThrow(new Error("Motorista não pode ter outra corrida com status accepted ou in_progress"));
 });
 
 test('Deve associar o driver_id na corrida e mudar o status para "accepted"', async () => {
